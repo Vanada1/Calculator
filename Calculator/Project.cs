761d2fd5ba499c9f60128b67f522deda3689e30e
Vanada1 <vovashvoev122300@gmail.com>
2020-09-25 11:49:21 +0700

    Добавлены xml-комментарии

--- Calculator/Project.cs
+++ Calculator/Project.cs
@@ -6,18 +6,33 @@ namespace Calculator
 {
 	public class Project
 	{
+		/// <summary>
+		/// Calculation list
+		/// </summary>
 		List<Calculator> Calculators { get; set; }
 
+		/// <summary>
+		/// Constructor for <see cref="Project"/> class
+		/// </summary>
+		/// <param name="calculators"></param>
 		public Project(List<Calculator> calculators)
 		{
 			Calculators = calculators;
 		}
 
+		/// <summary>
+		/// Default constructor for <see cref="Project"/> class
+		/// </summary>
 		public Project()
 		{
 			Calculators = new List<Calculator>();
 		}
 
+		/// <summary>
+		/// Searches all calculations for a specific date
+		/// </summary>
+		/// <param name="time"> date for search</param>
+		/// <returns>List of found calculations </returns>
 		public List<Calculator> GetDataByTime(DateTime time)
 		{
 			List<Calculator> foundCalculators = new List<Calculator>();
